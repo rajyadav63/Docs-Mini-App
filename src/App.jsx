fd36fa7c88c3a9e19315e10b28fe0194b2490545
rajyadav63 <vector36razz@gmail.com>
2025-06-13 02:38:44 +0530

    Day1 done

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      <h1 className='text-4xl bg-orange-500'>Docs Mini App</h1>
+    <div className='w-full h-screen bg-zinc-800'>
+      <h1 className='text-5xl bg-orange-600 text-white text-center'>Docs Mini App</h1>
     </div>
   )
 }
